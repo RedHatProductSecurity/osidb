fe1f5591105249b520e77c825e59333db1c973a6
Adrian Torres <atorresj@redhat.com>
2022-09-20 15:11:22 +0200

    Create management command for manual sync of flaws
    This commit introduces a custom Django management command `syncflaws`
    which accepts an arbitrary number of Bugzilla IDs to sync from BZ to
    OSIDB, this allows us to manually sync certain flaws after a bugfix from
    production without much fuss.
    
    Closes OSIDB-389

---
+++ osidb/management/commands/syncflaws.py
@@ -0,0 +1,34 @@
+import logging
+import traceback
+
+from django.core.management.base import BaseCommand
+
+from collectors.bzimport.collectors import FlawCollector
+
+
+class Command(BaseCommand):
+    help = "Synchronizes a list of Bugzilla IDs to OSIDB"
+
+    def add_arguments(self, parser):
+        parser.add_argument("bz_ids", nargs="+", type=int, help="List of bugzilla IDs")
+
+    def handle(self, *args, **options):
+        if options["verbosity"] != 3:
+            logging.disable(logging.CRITICAL)
+        fc = FlawCollector()
+        for bz_id in options["bz_ids"]:
+            self.stdout.write(
+                f"Synchronizing {bz_id}...",
+                ending="",
+            )
+            try:
+                fc.sync_flaw(str(bz_id))
+            except Exception as e:
+                self.stdout.write(self.style.ERROR("FAIL"))
+                if options["verbosity"] == 2:
+                    self.stdout.write(f"Exception: {e}")
+                elif options["verbosity"] == 3:
+                    self.stdout.write(traceback.format_exc())
+            else:
+                self.stdout.write(self.style.SUCCESS("OK"))
+        logging.disable(logging.NOTSET)
