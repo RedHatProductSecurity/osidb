93f5c909bc5f18b525c21e980ba3f7a8e179c21d
Adrian Torres <atorresj@redhat.com>
2023-01-19 17:49:39 +0100

    Apply multi-db routing for staging and production
    This commit sets the DATABASE_ROUTER for the staging and production
    settings to use the newly implemented WeightedReplicaRouter.

--- config/settings_stage.py
+++ config/settings_stage.py
@@ -83,8 +83,27 @@ DATABASES = {
             # the database or role level in managed databases such as AWS RDS
             "options": "-c osidb.acl=00000000-0000-0000-0000-000000000000",
         },
-    }
+    },
+    "read-replica-1": {
+        "NAME": get_env("OSIDB_DB_NAME", default="osidb"),
+        "USER": get_env("OSIDB_DB_USER"),
+        "PASSWORD": get_env("OSIDB_DB_PASSWORD"),
+        "HOST": get_env("OSIDB_DB_HOST_RO"),
+        "PORT": get_env("OSIDB_DB_PORT", default="5432"),
+        "ENGINE": "psqlextra.backend",
+        "OPTIONS": {
+            "sslmode": "require",
+            # prevent libpq from automatically trying to connect to the db via GSSAPI
+            "gssencmode": "disable",
+            # this is a hack due to our inability to set a custom parameter either at
+            # the database or role level in managed databases such as AWS RDS
+            "options": "-c osidb.acl=00000000-0000-0000-0000-000000000000",
+        },
+    },
 }
+
+# use a custom database router to effectively serve as a load balancer
+DATABASE_ROUTERS = ("osidb.routers.WeightedReplicaRouter",)
 
 STATIC_ROOT = "/opt/app-root/static/"
 STATIC_URL = "/static/"
