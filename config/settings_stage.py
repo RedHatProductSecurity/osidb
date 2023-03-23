f64048989d67d274b4862a057143ac210db30c3b
osoukup <osoukup@redhat.com>
2023-03-23 17:35:36 +0100

    make API requests transactional (OSIDB-232)

--- config/settings_stage.py
+++ config/settings_stage.py
@@ -71,6 +71,7 @@ DATABASES = {
         "HOST": get_env("OSIDB_DB_HOST"),
         "PORT": get_env("OSIDB_DB_PORT", default="5432"),
         "ENGINE": "psqlextra.backend",
+        "ATOMIC_REQUESTS": True,  # perform HTTP requests as atomic transactions
         "OPTIONS": {
             "sslmode": "require",
             # prevent libpq from automatically trying to connect to the db via GSSAPI
