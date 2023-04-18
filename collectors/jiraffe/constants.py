cfb4d4c68da51db47b546648cde49daad94ed2ca
osoukup <osoukup@redhat.com>
2023-04-18 09:42:16 +0200

    remove obsoleted Jiraffe constants

--- collectors/jiraffe/constants.py
+++ collectors/jiraffe/constants.py
@@ -3,17 +3,3 @@ from osidb.helpers import get_env
 # Auth
 JIRA_SERVER = get_env("JIRA_URL", default="https://issues.redhat.com")
 JIRA_TOKEN = get_env("JIRA_AUTH_TOKEN")
-
-# External resources
-PRODUCT_DEFINITIONS_URL = (
-    get_env("DASHBOARD_URL", "") + "/rest/api/latest/product-definitions"
-)
-
-# JIRA-specific
-JIRAFFE_AUTO_SYNC = get_env("JIRAFFE_AUTO_SYNC", default="True", is_bool=True)
-JIRA_SYNC_INTERVAL = "1m"
-
-# Celery task constants
-JIRA_MAX_TRIES = 3
-JIRA_SOFT_TIME_LIMIT = 240
-JIRA_RATE_LIMIT = get_env("OSIDB_JIRA_RATE_LIMIT", default="8/m")
