3c14c05978055beecbdc08ff3fd87098d201e0e6
Conrado Costa <concosta@redhat.com>
2023-08-22 19:13:35 -0400

    Add ps_constants models to osidb

--- osidb/models.py
+++ osidb/models.py
@@ -3087,6 +3087,36 @@ class FlawDraft(FlawMixin, AlertMixin):
         )
 
 
+class UbiPackage(models.Model):
+    """
+    An instance of this model represents one
+    entry in ubi packages list from PS_Constant project
+    """
+
+    # internal primary key
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # PsComponent name
+    name = models.CharField(max_length=255)
+
+    # major stream name (e.g. 'rhel-9') - Relates to PsUpdateStream any starting with this string (e.g. ['rhel-9', 'rhel-9.0', 'rhel-9.0.0.z])
+    major_stream_version = models.CharField(max_length=100)
+
+
+class SpecialConsiderationPackage(models.Model):
+    """
+    An instance of this model represents one
+    entry in special consideration packages list
+    from PS_Constant project
+    """
+
+    # internal primary key
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # PsComponent name
+    name = models.CharField(max_length=255, unique=True)
+
+
 from apps.bbsync.cc import AffectCCBuilder, RHSCLAffectCCBuilder  # noqa: E402
 
 from .constants import (  # noqa: E402
