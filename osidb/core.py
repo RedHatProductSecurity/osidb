46007fa7ee65add152776b127fcc8e796b12ac04
Adrian Torres <atorresj@redhat.com>
2023-09-13 12:44:09 +0200

    Disarm potential SQL injection vector
    Per Django's documentation [1] and generally any Python database
    driver library (such as psycopg2) it is generally a bad idea to directly
    do string interpolation when said interpolation consists of
    user-provided parameters into an SQL statement.
    
    Instead, users should:
    
    * Avoid manually quoting identifiers within the SQL statement
    * Pass any parameters as second argument of the execute/raw function
    
    While in theory this represents a vector for SQL Injection, it is not
    exploitable in practice because all input interpolated into the SQL
    statement is converted to UUIDs so there is no risk.
    
    [1] https://docs.djangoproject.com/en/3.2/topics/db/sql/#passing-parameters-into-raw

--- osidb/core.py
+++ osidb/core.py
@@ -32,10 +32,10 @@ def set_user_acls(groups) -> None:
     try:
         acls = generate_acls(groups)
         if acls:
-            stmt = "SET osidb.acl='%s';"  # This is an identifier, not an ACL / list of permissions
             # in theory psycopg2 should support list conversion but it seems to be broken,
             # to pass into postgres we need to convert UUID[] into STRING[]
             # doing some old fashioned string munging TBD-INVESTIGATE psycopg2
-            connection.cursor().execute(stmt % ",".join(acls))
+            with connection.cursor() as cursor:
+                cursor.execute("SET osidb.acl = %s", [",".join(acls)])
     except Exception:
         raise OSIDBException("Cannot set user acl")
