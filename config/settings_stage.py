b6bbbe8ac1eae4705ade9d5e844df2240c926aa3
Adrian Torres <atorresj@redhat.com>
2023-09-18 16:43:53 +0200

    Pool DB connections for 60s

--- config/settings_stage.py
+++ config/settings_stage.py
@@ -83,6 +83,7 @@ DATABASES = {
             # the database or role level in managed databases such as AWS RDS
             "options": "-c osidb.acl=00000000-0000-0000-0000-000000000000",
         },
+        "CONN_MAX_AGE": 60,
     },
     "read-replica-1": {
         "NAME": get_env("OSIDB_DB_NAME", default="osidb"),
@@ -99,6 +100,7 @@ DATABASES = {
             # the database or role level in managed databases such as AWS RDS
             "options": "-c osidb.acl=00000000-0000-0000-0000-000000000000",
         },
+        "CONN_MAX_AGE": 60,
     },
 }
 
