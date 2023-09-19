3f856392b1ab497c2af1b2d2c3ff9bd6e0987af3
Adrian Torres <atorresj@redhat.com>
2023-09-19 14:06:58 +0200

    Move monkey-patching around

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,6 +4,14 @@ worker_class = "gevent"
 proc_name = "osidb"
 timeout = 300
 
+
+def post_fork(server, worker):
+    from psycogreen.gevent import patch_psycopg
+
+    patch_psycopg()
+    worker.log.info("Made psycopg2 green")
+
+
 errorlog = "-"
 # Make sure wsgi.url_scheme gets set to HTTPS, by trusting the X_FORWARDED_PROTO header set by the proxy
 forwarded_allow_ips = "*"
