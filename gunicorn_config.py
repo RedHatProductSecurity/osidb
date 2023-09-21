93df2f28280dfe8b3261c3b57596786bc22b0db8
Adrian Torres <atorresj@redhat.com>
2023-09-21 12:12:07 +0200

    try threaded workers

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,20 +1,12 @@
 bind = "0.0.0.0:8000"
-worker_class = "gevent"
-workers = 3
-worker_connections = 2000
+worker_class = "gthread"
+workers = 4
+threads = 4
 proc_name = "osidb"
 timeout = 300
 graceful_timeout = 800
 reuse_port = True
 preload_app = True
-
-
-def post_fork(server, worker):
-    from psycogreen.gevent import patch_psycopg
-
-    patch_psycopg()
-    worker.log.info("Made psycopg2 green")
-
 
 errorlog = "-"
 # Make sure wsgi.url_scheme gets set to HTTPS, by trusting the X_FORWARDED_PROTO header set by the proxy
