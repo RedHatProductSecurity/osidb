6f37db1cf281e4ae1c17894c1ee42aadd2073708
Jitka Obselkova <jobselko@redhat.com>
2023-11-30 17:49:44 +0100

    Add methods for Snippet to Flaw conversion

--- osidb/tests/test_snippet.py
+++ osidb/tests/test_snippet.py
@@ -3,8 +3,9 @@ import uuid
 import pytest
 from django.conf import settings
 
+from apps.osim.workflow import WorkflowModel
 from osidb.core import generate_acls
-from osidb.models import FlawCVSS, FlawReference, Snippet
+from osidb.models import Flaw, FlawCVSS, FlawReference, FlawSource, FlawType, Snippet
 
 pytestmark = pytest.mark.unit
 
@@ -56,3 +57,80 @@ class TestSnippet:
             uuid.UUID(acl) for acl in generate_acls([settings.INTERNAL_WRITE_GROUP])
         ]
         assert Snippet.objects.count() == 1
+
+    def test_create_flaw_from_snippet(self):
+        """
+        Tests the creation of a flaw from a snippet.
+        """
+        snippet = get_snippet()
+        content = snippet.content
+
+        created_flaw = snippet._create_flaw()
+
+        all_flaws = Flaw.objects.filter(cve_id=content["cve_id"])
+        assert all_flaws.count() == 1
+
+        flaw = all_flaws.first()
+        assert flaw == created_flaw
+        assert flaw.cve_id == content["cve_id"]
+        assert flaw.cvss_scores.count() == 1
+        assert flaw.cwe_id == content["cwe_id"]
+        assert flaw.description == content["description"]
+        assert flaw.osim_state == WorkflowModel.OSIMState.NEW
+        assert flaw.references.count() == 1
+        assert flaw.snippets.count() == 0
+        assert flaw.source == snippet.source
+        assert flaw.title == content["title"]
+        assert flaw.type == FlawType.VULNERABILITY
+
+        flaw_cvss = flaw.cvss_scores.all().first()
+        assert flaw_cvss.issuer == FlawCVSS.CVSSIssuer.NIST
+        assert flaw_cvss.score == 8.1
+        assert flaw_cvss.vector == "CVSS:3.1/AV:N/AC:H/PR:N/UI:N/S:U/C:H/I:H/A:H"
+        assert flaw_cvss.version == FlawCVSS.CVSSVersion.VERSION3
+
+        flaw_ref = flaw.references.all().first()
+        assert flaw_ref.type == FlawReference.FlawReferenceType.SOURCE
+        assert flaw_ref.url == "https://nvd.nist.gov/vuln/detail/CVE-2023-0001"
+
+        # check ACLs
+        assert flaw.acl_read == [
+            uuid.UUID(acl) for acl in generate_acls([settings.INTERNAL_READ_GROUP])
+        ]
+        assert flaw.acl_write == [
+            uuid.UUID(acl) for acl in generate_acls([settings.INTERNAL_WRITE_GROUP])
+        ]
+        assert flaw.acl_read == flaw_cvss.acl_read == flaw_ref.acl_read
+        assert flaw.acl_write == flaw_cvss.acl_write == flaw_ref.acl_write
+
+    @pytest.mark.parametrize(
+        "cve_id,has_flaw,has_snippet",
+        [
+            (None, False, False),
+            ("CVE-2023-0001", False, False),
+            ("CVE-2023-0001", True, False),
+            ("CVE-2023-0001", True, True),
+        ],
+    )
+    def test_convert_snippet_to_flaw(self, cve_id, has_flaw, has_snippet):
+        """
+        Tests the conversion of a snippet into a flaw and their linking.
+        """
+        snippet = get_snippet(cve_id)
+
+        if has_flaw:
+            f = snippet._create_flaw()
+            if has_snippet:
+                snippet.flaw = f
+                snippet.save()
+
+        snippet.convert_snippet_to_flaw()
+
+        flaws = Flaw.objects.filter(cve_id=cve_id, source=FlawSource.NVD)
+        assert flaws.count() == 1
+
+        flaw = flaws.first()
+        assert flaw.snippets.count() == 1
+        assert flaw.snippets.first() == snippet
+
+        assert snippet.flaw == flaw
