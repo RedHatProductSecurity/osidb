062f6af03adc28baa74b16fef417e95b01c112cb
Adrian Torres <atorresj@redhat.com>
2024-01-22 13:41:03 +0100

    Change locustfile to be more representative of real-world scenarios
    These requests are made by real clients in production and are more
    representative of the average workload seen in OSIDB.
    
    That said, these represent a worst-case scenario and show to be
    manageable under heavy load of 10 concurrent users.

--- perf/main.py
+++ perf/main.py
@@ -1,19 +1,47 @@
-from locust import HttpUser, constant_pacing, task
+from locust import HttpUser, between, constant_pacing, task
 
 
-class ChangedFlawsUser(HttpUser):
-    wait_time = constant_pacing(1)
-
-    @task
-    def changed_after(self):
-        self.client.get("/osidb/api/v1/flaws?changed_after=2022-12-01T00:00:00Z")
-
-
-class ChangedIncludeFlawsUser(HttpUser):
-    wait_time = constant_pacing(1)
+class SFM2User(HttpUser):
+    wait_time = between(1, 3)
+    weight = 3
 
     @task
-    def changed_after(self):
+    def get_nvd_cvss_scores(self):
+        self.client.get("/osidb/api/v1/flaws?include_fields=cve_id,nvd_cvss2,nvd_cvss3")
+
+
+class SDEngineUser(HttpUser):
+    wait_time = between(1, 3)
+    weight = 3
+
+    @task(1)
+    def get_status(self):
+        payload = {"username": "testuser", "password": "password"}
+        headers = {"Content-Type": "application/json"}
+        with self.client.post("/auth/token", json=payload, headers=headers) as r:
+            token = r.json()["access"]
         self.client.get(
-            "/osidb/api/v1/flaws?include_fields=cve_id&changed_after=2022-12-01T00:00:00Z"
+            "/collectors/api/v1/status",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+
+    @task(4)
+    def get_cve_ids(self):
+        self.client.get("/osidb/api/v1/flaws?include_fields=cve_id")
+
+    @task(5)
+    def get_cve_data(self):
+        self.client.get(
+            "/osidb/api/v1/flaws?include_meta_attr=mitigation,cvss3_comment,bz_id,bz_summary&exclude_fields=comments,classification&changed_after=2015-01-01T09:00:00Z&limit=60"
+        )
+
+
+class GriffonUser(HttpUser):
+    wait_time = between(1, 3600)
+    weight = 1
+
+    @task
+    def get_affects_from_ps_product(self):
+        self.client.get(
+            "/osidb/api/v1/flaws?affects__affectedness=AFFECTED&affects__ps_module=cost-management&affects__resolution=FIX&include_fields=cve_id,title,resolution,impact,affects&limit=50"
         )
