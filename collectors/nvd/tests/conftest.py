6cd7138350593c63e786be2a8ae498d5daba3ac0
Jitka Obselkova <jobselko@redhat.com>
2024-02-16 14:07:58 +0100

    Refactor and extend tests for NVD

--- collectors/nvd/tests/conftest.py
+++ collectors/nvd/tests/conftest.py
@@ -1,6 +1,21 @@
+import uuid
+
 import pytest
+from django.conf import settings
+
+from osidb.core import generate_acls
 
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
+
+
+@pytest.fixture
+def internal_read_groups():
+    return [uuid.UUID(acl) for acl in generate_acls([settings.INTERNAL_READ_GROUP])]
+
+
+@pytest.fixture
+def internal_write_groups():
+    return [uuid.UUID(acl) for acl in generate_acls([settings.INTERNAL_WRITE_GROUP])]
