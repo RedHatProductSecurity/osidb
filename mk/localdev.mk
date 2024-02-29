RedHatProductSecurity/osidb

commit d1e4804b553ea0f32e248dfc1f4a8e73af1d1893

Simplify docker-compose of celery workers
This commit changes the way that the celery workers are setup for the
local dev environment by leveraging docker-compose/podman-compose's
deploy.replicas feature.

By default, 2 celery workers are instantiated when running `make
start-local`, however the amount of celery workers to use can be changed
by simply setting the OSIDB_CELERY_WORKERS_NO environment variable.

This simplifies the docker-compose file and gives developers more
flexibility when running the application locally.

docker-compose.yml
@@ -125,38 +125,12 @@ services:
           -c ssl_key_file=/var/lib/postgresql/local-server.key
           -c config_file=/etc/postgresql/postgresql.conf
 
-
-    celery_host1:
-      container_name: celery_host1
-      hostname: celery_host1
+    celery:
       image: osidb-service
       command: ./scripts/run-celery.sh
-      volumes:
-        - ${PWD}:/opt/app-root/src:z
-      environment:
-        DJANGO_SETTINGS_MODULE: "config.settings_local"
-        OSIDB_DEBUG: ${OSIDB_DEBUG}
-        BZIMPORT_BZ_API_KEY: ${BZIMPORT_BZ_API_KEY:?Variable BZIMPORT_BZ_API_KEY must be set.}
-        BZIMPORT_BZ_URL: ${BZIMPORT_BZ_URL}
-        JIRA_AUTH_TOKEN: ${JIRA_AUTH_TOKEN:?Variable JIRA_AUTH_TOKEN must be set.}
-        JIRA_URL: ${JIRA_URL}
-        ET_URL: ${ET_URL}
-        PRODUCT_DEF_URL: ${PRODUCT_DEF_URL}
-      depends_on: ["osidb-data", "osidb-service", "redis"]
-      # See "NOTE about healthchecks":
-      # depends_on:
-      #   osidb-data:
-      #     condition: service_healthy
-      #   osidb-service:
-      #     condition: service_healthy
-      #   redis:
-      #     condition: service_started
-
-    celery_host2:
-      container_name: celery_host2
-      hostname: celery_host2
-      image: osidb-service
-      command: ./scripts/run-celery.sh
+      deploy:
+        mode: replicated
+        replicas: ${OSIDB_CELERY_WORKERS_NO:-2}
       volumes:
         - ${PWD}:/opt/app-root/src:z
       environment:

commit 933935c0b020c79bba27524e7d2e24ba7f0754ee

Fix applying requirements.txt on celery hosts Bug introduced in commit d1e4804b553ea0f32e248dfc1f4a8e73af1d1893

docker-compose.yml
@@ -48,7 +48,7 @@ version: '3'
 # 	@cnt=1; until { $(podman) healthcheck run osidb-data >/dev/null 2>&1 ; } ; do ((cnt++)); echo ">waiting for osidb-data" ; sleep 2 ; if (( cnt > 30 )) ; then echo ">Container osidb-data seems stuck. Let's restart it. Sorry, IDK why that sometimes happens. It seems to happen less if 'make stop-local' is run first." ; $(podman) restart osidb-data || true ; sleep 10 ; $(podman) start osidb-data || true ; cnt=1 ; fi ; done
 # 	@$(podmancompose) -f docker-compose.yml -f docker-compose.test.yml up -d osidb-service testldap || $(WORKAROUND_B) || true
 # 	@cnt=1; until { $(podman) healthcheck run osidb-service >/dev/null 2>&1 ; } ; do ((cnt++)); echo ">waiting for osidb-service" ; sleep 2 ; if (( cnt > 30 )) ; then echo ">Container osidb-service seems stuck. Let's restart it. Sorry, IDK why that sometimes happens. It seems to happen less if 'make stop-local' is run first." ; $(podman) restart osidb-service || true ; sleep 10 ; $(podman) start osidb-service || true ; cnt=1 ; fi ; done
-# 	@$(podmancompose) -f docker-compose.yml -f docker-compose.test.yml up -d celery_host1 celery_host2 celery_beat flower
+# 	@$(podmancompose) -f docker-compose.yml -f docker-compose.test.yml up -d celery celery_beat flower
 # 	@$(podmancompose) -f docker-compose.yml -f docker-compose.test.yml up -d || $(WORKAROUND_C)
 # [END COMPOSE UP SHOWCASE]
 

mk/localdev.mk
@@ -30,7 +30,7 @@ start-local-psql: check-reg check-venv generate_local_pg_tls_cert
 .PHONY : stop-local
 stop-local:
 	@echo ">stopping local env without deleting containers"
-	$(podmancompose) -f docker-compose.yml -f docker-compose.test.yml stop || $(podmancompose) -f docker-compose.yml -f docker-compose.test.yml stop testrunner phpldapadmin testldap flower redis celery_beat celery_host1 celery_host2 osidb-service osidb-data
+	$(podmancompose) -f docker-compose.yml -f docker-compose.test.yml stop || $(podmancompose) -f docker-compose.yml -f docker-compose.test.yml stop testrunner phpldapadmin testldap flower redis celery_beat celery osidb-service osidb-data
 
 
 

mk/setup.mk
@@ -85,10 +85,10 @@ upgrade-dep: check-venv-active
 apply-requirements-txt: check-reg check-venv sync-deps compose-up
 	@echo ">appyling requirements.txt on osidb-service"
 	$(podman) exec -it osidb-service pip3 install -r /opt/app-root/src/requirements.txt
-	@echo ">appyling requirements.txt on celery_host1"
-	$(podman) exec -it celery_host1 pip3 install -r /opt/app-root/src/requirements.txt
-	@echo ">appyling requirements.txt on celery_host2"
-	$(podman) exec -it celery_host2 pip3 install -r /opt/app-root/src/requirements.txt
+	@echo ">appyling requirements.txt on osidb_celery_1"
+	$(podman) exec -it osidb_celery_1 pip3 install -r /opt/app-root/src/requirements.txt
+	@echo ">appyling requirements.txt on osidb_celery_2" # if you have more celery replicas, you're on your own
+	$(podman) exec -it osidb_celery_2 pip3 install -r /opt/app-root/src/requirements.txt || true  # do not fail if only 1 host is configured
 	@echo ">appyling requirements.txt on celery_beat"
 	$(podman) exec -it celery_beat pip3 install -r /opt/app-root/src/requirements.txt
 	@echo ">appyling requirements.txt on flower"