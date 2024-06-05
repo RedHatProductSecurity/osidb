cadc85fcee810088870beb2ffed0bd1641cc5478
Jakub Frejlach <jfrejlac@redhat.com>
2024-06-05 15:16:27 +0200

    Sanitize Trackers app serializers
    Some of these serializers were using incorrect fields and had incorrecly
    decoreated OpenAPI schema extension.
    
    Also there was a pretty extra boiler plate code regarding
    include_meta_attr which was a result of a bug in the IncludeMetaAttrMixin
    which was addressed in the previous commit.

--- apps/trackers/serializer.py
+++ apps/trackers/serializer.py
@@ -1,7 +1,6 @@
 """
 Taskman requests serializers
 """
-from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from osidb.serializer import AffectSerializer
@@ -21,39 +20,14 @@ class PsStreamSelectionSerializer(serializers.Serializer):
     aus = serializers.BooleanField()
 
 
-@extend_schema_field(AffectSerializer())
 class ModuleComponentSerializer(serializers.Serializer):
-    ps_module = serializers.UUIDField(format="hex_verbose")
-    ps_component = serializers.UUIDField(format="hex_verbose")
-    streams = serializers.ListField(child=PsStreamSelectionSerializer())
+    ps_module = serializers.CharField()
+    ps_component = serializers.CharField()
+    streams = PsStreamSelectionSerializer(many=True)
     selected = serializers.BooleanField()
-    affect = serializers.SerializerMethodField()
-
-    def get_affect(self, obj):
-        """affects serializer getter"""
-        context = {
-            "include_fields": [],
-            "exclude_fields": [],
-            "include_meta_attr": [],
-        }
-
-        serializer = AffectSerializer(instance=obj["affect"], context=context)
-        return serializer.data
+    affect = AffectSerializer()
 
 
-@extend_schema_field(AffectSerializer(many=True))
 class TrackerSuggestionSerializer(serializers.Serializer):
-    modules_components = serializers.ListField(child=ModuleComponentSerializer())
-    not_applicable = serializers.SerializerMethodField()
-
-    def get_not_applicable(self, obj):
-        """affects serializer getter"""
-        affects = obj["not_applicable"].all()
-        context = {
-            "include_fields": [],
-            "exclude_fields": [],
-            "include_meta_attr": [],
-        }
-
-        serializer = AffectSerializer(instance=affects, many=True, context=context)
-        return serializer.data
+    modules_components = ModuleComponentSerializer(many=True)
+    not_applicable = AffectSerializer(many=True)
