7bec66812d7512f1cb91ce4df319e4e785b63a6f
Jakub Frejlach <jfrejlac@redhat.com>
2024-06-13 17:42:00 +0200

    Account for trailing slash in install-certs.sh
    When using URL without trailing slash it used to result
    in a malformed url (eg. pathto/certs instead of path/to/certs)

--- scripts/install-certs.sh
+++ scripts/install-certs.sh
@@ -3,9 +3,9 @@
 if [[ -z "${1}" ]]; then
     echo -e "\e[1;33mWARNING: RH_CERT_URL environment variable not set, internal RH resources won't be accessible\e[0m"
 else
-    curl "${1}certs/2015-IT-Root-CA.pem" -o /etc/pki/ca-trust/source/anchors/RH-IT-Root-CA.crt
-    curl "${1}certs/2022-IT-Root-CA.pem" -o /etc/pki/ca-trust/source/anchors/2022-IT-Root-CA.pem
+    curl "${1%/}/certs/2015-IT-Root-CA.pem" -o /etc/pki/ca-trust/source/anchors/RH-IT-Root-CA.crt
+    curl "${1%/}/certs/2022-IT-Root-CA.pem" -o /etc/pki/ca-trust/source/anchors/2022-IT-Root-CA.pem
     mkdir /etc/ipa
-    curl "${1}chains/ipa-ca-chain-2015.crt" -o /etc/ipa/ipa.crt
+    curl "${1%/}/chains/ipa-ca-chain-2015.crt" -o /etc/ipa/ipa.crt
     update-ca-trust
 fi
