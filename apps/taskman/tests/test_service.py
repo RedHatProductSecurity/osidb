46ef7a728df13c9686bd66ffd104a94550b19c4c
Conrado Costa <concosta@redhat.com>
2024-07-17 09:18:16 -0400

    Allow Flaw API to proper unassign owner in Jira

--- apps/taskman/tests/test_service.py
+++ apps/taskman/tests/test_service.py
@@ -9,6 +9,7 @@ import pytest
 
 from apps.taskman.service import JiraTaskmanQuerier
 from apps.workflows.workflow import WorkflowModel
+from osidb.models import Flaw
 from osidb.tests.factories import AffectFactory, FlawFactory
 
 pytestmark = pytest.mark.unit
@@ -28,7 +29,12 @@ class TestTaskmanService(object):
         Test that service is able to create and update regular fields, team, assignment and status
         """
         # Remove randomness to reuse VCR every possible time
-        flaw = FlawFactory(embargoed=False, uuid="9d9b3b14-0c44-4030-883c-8610f7e2879b")
+        flaw = FlawFactory(
+            embargoed=False,
+            uuid="9d9b3b14-0c44-4030-883c-8610f7e2879b",
+            workflow_state=WorkflowModel.WorkflowState.NEW,
+        )
+        AffectFactory(flaw=flaw)
         taskman = JiraTaskmanQuerier(token=user_token)
 
         response1 = taskman.create_or_update_task(flaw=flaw)
@@ -40,14 +46,17 @@ class TestTaskmanService(object):
         flaw.title = new_title
         flaw.owner = "concosta@redhat.com"
         flaw.team_id = "2861"
+        flaw.workflow_state = WorkflowModel.WorkflowState.TRIAGE
         flaw.save()
 
         response2 = taskman.create_or_update_task(flaw=flaw)
+        status, _ = flaw.jira_status()
         assert response2.status_code == 200
         assert response2.data["fields"]["summary"] == new_title
         assert response2.data["fields"]["customfield_12313240"]["id"] == 2861
         assert response2.data["fields"]["customfield_12313240"]["name"] == "OSIDB"
         assert response2.data["fields"]["assignee"]["name"] == "concosta@redhat.com"
+        assert response2.data["fields"]["status"]["name"] == status
 
         assert flaw.workflow_state == WorkflowModel.WorkflowState.TRIAGE
         flaw.workflow_state = WorkflowModel.WorkflowState.PRE_SECONDARY_ASSESSMENT
@@ -56,6 +65,16 @@ class TestTaskmanService(object):
         assert response3.status_code == 200
         status, _ = flaw.jira_status()
         assert response3.data["fields"]["status"]["name"] == status
+
+        # test unassign
+        flaw.owner = ""
+        flaw.save(raise_validation_error=False)
+        response4 = taskman.create_or_update_task(flaw=flaw)
+        assert response4.status_code == 200
+        flaw = Flaw.objects.get(uuid=flaw.uuid)
+        assert flaw.owner == ""
+        issue = taskman.jira_conn.issue(flaw.task_key).raw
+        assert not issue["fields"]["assignee"]
 
     @pytest.mark.vcr
     def test_comments(self, user_token):
