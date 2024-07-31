c2bf78cecead672c4d0d38507d78cb9b5af7fea9
osoukup <osoukup@redhat.com>
2024-07-31 17:09:01 +0200

    rollback transaction when handling exception

--- osidb/exception_handlers.py
+++ osidb/exception_handlers.py
@@ -2,6 +2,7 @@ import logging
 
 from bugzilla.exceptions import BugzillaError
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db import transaction
 from django.db.utils import OperationalError
 from jira.exceptions import JIRAError
 from rest_framework import status
@@ -15,6 +16,7 @@ logger = logging.getLogger(__name__)
 
 def exception_handler(exc, context):
     if http_code := getattr(type(exc), "http_code", False):
+        transaction.set_rollback(True)
         # serialize custom exceptions when raised as part of the
         # request-response lifecycle by using an `http_code` class variable
         # as an interface
@@ -25,16 +27,19 @@ def exception_handler(exc, context):
         return Response(response_data, status=http_code)
 
     if isinstance(exc, BugzillaError):
+        transaction.set_rollback(True)
         logger.exception(exc)
         data = {"detail": str(exc)}
         return Response(data, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     if isinstance(exc, JIRAError):
+        transaction.set_rollback(True)
         logger.exception(exc)
         data = {"detail": exc.text}
         return Response(data, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     if isinstance(exc, OperationalError):
+        transaction.set_rollback(True)
         logger.exception(exc)
         details = str(exc)
         if "deadlock" in details:
