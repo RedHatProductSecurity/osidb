fe59c4e6c501be2d53ee4878363a7544b162ac3c
Jitka Obselkova <jobselko@redhat.com>
2024-09-04 14:01:48 +0200

    Set False as default value for CVEorg collector

--- collectors/cveorg/constants.py
+++ collectors/cveorg/constants.py
@@ -2,7 +2,7 @@ from osidb.helpers import get_env
 
 # Switch to turn the collector on/off
 CVEORG_COLLECTOR_ENABLED = get_env(
-    "CVEORG_COLLECTOR_ENABLED", default="True", is_bool=True
+    "CVEORG_COLLECTOR_ENABLED", default="False", is_bool=True
 )
 
 CELERY_PVC_PATH = get_env("CELERY_PVC_PATH", default="/opt/app-root/output-files")
