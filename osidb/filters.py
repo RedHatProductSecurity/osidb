b3dc2abe8b3fff3c10c8ce5eb044870a9b8a96c2
Alvaro Tinoco <atinocom@redhat.com>
2024-09-26 14:21:33 +0200

    Create custom lookup for components field

--- osidb/filters.py
+++ osidb/filters.py
@@ -16,7 +16,7 @@ from django_filters.rest_framework import (
     OrderingFilter,
 )
 from djangoql.queryset import apply_search
-from djangoql.schema import DjangoQLSchema
+from djangoql.schema import DjangoQLSchema, StrField
 
 from apps.workflows.workflow import WorkflowModel
 from osidb.dmodels.package_versions import Package
@@ -292,8 +292,43 @@ class FlawQLSchema(DjangoQLSchema):
         exclude = ["acl_read", "acl_write"]
         if model == Flaw:
             exclude += ["snippets", "local_updated_dt"]
-
+            fields.remove("components")
+            fields += [FlawComponentField()]
         return set(fields) - set(exclude)
+
+
+class FlawComponentField(StrField):
+    model = Flaw
+    name = "components"
+    suggest_options = True
+
+    def get_options(self, search):
+        options = super(FlawComponentField, self).get_options(search)
+        flat_list = []
+        for option in options:
+            flat_list += option
+        return flat_list
+
+    def get_lookup(self, path, operator, value):
+        lookup = "contains" if len(value) > 1 else "exact"
+        value = [component for component in value.split(",") if component]
+
+        if operator == "in":
+            result = None
+            for component in value:
+                condition = self.get_lookup(path, "=", component)
+                result = condition if result is None else result | condition
+            return result
+        elif operator == "not in":
+            result = None
+            for component in value:
+                condition = self.get_lookup(path, "!=", component)
+                result = condition if result is None else result & condition
+            return result
+        elif operator == "!=":
+            return ~Q(**{f"components__{lookup}": value})
+        elif operator == "=":
+            return Q(**{f"components__{lookup}": value})
 
 
 class FlawFilter(DistinctFilterSet, IncludeFieldsFilterSet, ExcludeFieldsFilterSet):
