f8fe8ca96d82b760a65f0acde08d3e4490baf99f
osoukup <osoukup@redhat.com>
2024-10-17 12:45:27 +0200

    modify PS module support test to test ProdSec support

--- osidb/models/tests/test_ps_module.py
+++ osidb/models/tests/test_ps_module.py
@@ -8,7 +8,7 @@ from osidb.models import PsModule
 class TestPsModule:
     @freeze_time(timezone.datetime(2020, 10, 20, tzinfo=timezone.utc))
     @pytest.mark.parametrize(
-        "supported_from,supported_until,is_supported",
+        "supported_from,supported_until,is_prodsec_supported",
         [
             (
                 timezone.datetime(2020, 10, 10, tzinfo=timezone.utc),
@@ -23,7 +23,7 @@ class TestPsModule:
             (
                 timezone.datetime(2020, 10, 30, tzinfo=timezone.utc),
                 timezone.datetime(2020, 10, 30, tzinfo=timezone.utc),
-                False,
+                True,  # support start in the future should not restrict ProdSec support
             ),
             (
                 None,
@@ -37,11 +37,13 @@ class TestPsModule:
             ),
         ],
     )
-    def test_is_supported(self, supported_from, supported_until, is_supported):
+    def test_is_prodsec_supported(
+        self, supported_from, supported_until, is_prodsec_supported
+    ):
         assert (
             PsModule(
                 supported_from_dt=supported_from,
                 supported_until_dt=supported_until,
-            ).is_supported
-            == is_supported
+            ).is_prodsec_supported
+            == is_prodsec_supported
         )
