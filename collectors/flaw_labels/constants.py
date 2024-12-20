6a569e30067d60abf33f1e9141db6575866f4b77
Alvaro Tinoco <atinocom@redhat.com>
2024-12-20 14:53:40 +0100

    Set default env value

--- collectors/flaw_labels/constants.py
+++ collectors/flaw_labels/constants.py
@@ -4,5 +4,5 @@ flaw label collector constants
 
 from osidb.helpers import get_env
 
-FLAW_LABELS_REPO_URL = get_env("FLAW_LABELS_URL")
+FLAW_LABELS_REPO_URL = get_env("FLAW_LABELS_URL", "")
 FLAW_LABELS_REPO_BRANCH = "main"
