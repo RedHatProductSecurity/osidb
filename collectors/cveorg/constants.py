ee49428d7328377721bccb752ac26357438bf127
Jitka Obselkova <jobselko@redhat.com>
2025-01-06 16:38:04 +0100

    Set CVEorg to be enabled by default

--- collectors/cveorg/constants.py
+++ collectors/cveorg/constants.py
@@ -2,7 +2,7 @@ from osidb.helpers import get_env, get_env_date
 
 # Switch to turn the collector on/off
 CVEORG_COLLECTOR_ENABLED = get_env(
-    "CVEORG_COLLECTOR_ENABLED", default="False", is_bool=True
+    "CVEORG_COLLECTOR_ENABLED", default="True", is_bool=True
 )
 
 CVEORG_START_DATE = get_env_date("CVEORG_START_DATE", default="2024-10-01")
