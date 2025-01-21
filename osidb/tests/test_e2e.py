6dc7abf8c51a09ee5b620768e4e805df688f2acd
Conrado Costa <concosta@redhat.com>
2025-01-21 18:00:30 -0500

    add e2e test for externally collected flaws

--- osidb/tests/test_e2e.py
+++ osidb/tests/test_e2e.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from django.utils import timezone
 from freezegun import freeze_time
@@ -6,8 +8,11 @@ from apps.taskman.service import JiraTaskmanQuerier
 from apps.trackers.constants import TRACKERS_API_VERSION
 from apps.workflows.models import Workflow
 from apps.workflows.workflow import WorkflowFramework, WorkflowModel
+from collectors.cveorg import tests as cveorg_tests
+from collectors.cveorg.collectors import CVEorgCollector
+from collectors.cveorg.models import Keyword
 from collectors.jiraffe.collectors import JiraTrackerCollector
-from osidb.models import Affect, Flaw, PsUpdateStream, Tracker
+from osidb.models import Affect, Flaw, PsUpdateStream, Snippet, Tracker
 from osidb.sync_manager import (
     BZSyncManager,
     JiraTaskSyncManager,
@@ -20,6 +25,48 @@ pytestmark = pytest.mark.unit
 @pytest.fixture
 def test_trackers_api_uri() -> str:
     return f"http://osidb-service:8000/trackers/api/{TRACKERS_API_VERSION}"
+
+
+@pytest.fixture()
+def mock_keywords() -> None:
+    """
+    Set testing keywords to mock the ones from the ps-constants repository.
+    """
+    Keyword(keyword="kernel", type=Keyword.Type.ALLOWLIST).save()
+    Keyword(keyword=r"(?:\W|^)\.NET\b", type=Keyword.Type.ALLOWLIST_SPECIAL_CASE).save()
+    Keyword(keyword=".*plugin.*for WordPress", type=Keyword.Type.BLOCKLIST).save()
+    Keyword(keyword="Cisco", type=Keyword.Type.BLOCKLIST).save()
+    Keyword(keyword="IBM Tivoli", type=Keyword.Type.BLOCKLIST).save()
+    Keyword(keyword="iTunes", type=Keyword.Type.BLOCKLIST).save()
+    Keyword(keyword="iOS", type=Keyword.Type.BLOCKLIST_SPECIAL_CASE).save()
+
+
+@pytest.fixture()
+def mock_repo(monkeypatch) -> None:
+    """
+    Set testing data and variables to mock the cvelistV5 repository.
+    """
+    repo_path = f"{Path(cveorg_tests.__file__).resolve().parent}/cvelistV5"
+    cve_path = r"CVE-(?:1999|2\d{3})-(?!0{4})(?:0\d{3}|[1-9]\d{3,}).json$"
+
+    def clone_repo(self):
+        return
+
+    def update_repo(self):
+        return
+
+    def get_repo_changes(self):
+        stdout = "CVE-2024-0181.json\nCVE-2024-0203.json\nCVE-2024-1087.json\nCVE-2024-4923.json\n"
+        period_end = timezone.datetime(
+            2024, 7, 1, tzinfo=timezone.get_current_timezone()
+        )
+        return stdout, period_end
+
+    monkeypatch.setattr(CVEorgCollector, "REPO_PATH", repo_path)
+    monkeypatch.setattr(CVEorgCollector, "CVE_PATH", cve_path)
+    monkeypatch.setattr(CVEorgCollector, "clone_repo", clone_repo)
+    monkeypatch.setattr(CVEorgCollector, "update_repo", update_repo)
+    monkeypatch.setattr(CVEorgCollector, "get_repo_changes", get_repo_changes)
 
 
 def tzdatetime(*args):
@@ -287,3 +334,313 @@ class TestE2E:
         assert any(
             h["pgh_diff"] and "task_key" in h["pgh_diff"] for h in body["history"]
         )
+
+    @pytest.mark.vcr
+    @freeze_time(tzdatetime(2024, 8, 6))
+    def test_cveorg_with_workflows(
+        self,
+        auth_client,
+        bugzilla_token,
+        client,
+        enable_bz_async_sync,
+        enable_jira_task_async_sync,
+        enable_jira_tracker_sync,
+        jira_token,
+        mock_keywords,
+        mock_repo,
+        monkeypatch,
+        test_api_uri,
+    ):
+        """
+        Test that snippets and flaws are created correctly and can
+        be edited and promoted until closed
+        """
+        # Simulates user behavior for task sync
+        monkeypatch.setattr(JiraTaskmanQuerier, "is_service_account", lambda x: False)
+
+        # 1) setup workflows for test
+        workflow_framework = WorkflowFramework()
+        workflow_framework._workflows = []
+
+        state_new = {
+            "name": WorkflowModel.WorkflowState.NEW,
+            "requirements": [],
+            "jira_state": "New",
+            "jira_resolution": None,
+        }
+
+        state_triage = {
+            "name": WorkflowModel.WorkflowState.TRIAGE,
+            "requirements": ["has owner"],
+            "jira_state": "Refinement",
+            "jira_resolution": None,
+        }
+
+        state_sec = {
+            "name": WorkflowModel.WorkflowState.SECONDARY_ASSESSMENT,
+            "requirements": ["has owner"],
+            "jira_state": "In Progress",
+            "jira_resolution": None,
+        }
+
+        state_done = {
+            "name": WorkflowModel.WorkflowState.DONE,
+            "requirements": [
+                {
+                    "condition": "OR",
+                    "requirements": [
+                        "has trackers",
+                        "impact is low",
+                        "impact is moderate",
+                    ],
+                }
+            ],
+            "jira_state": "Closed",
+            "jira_resolution": "Done",
+        }
+
+        workflow = Workflow(
+            {
+                "name": "DEFAULT",
+                "description": "random description",
+                "priority": 1,
+                "conditions": [],
+                "states": [state_new, state_triage, state_sec, state_done],
+            }
+        )
+
+        state_reject = {
+            "name": WorkflowModel.WorkflowState.REJECTED,
+            "requirements": [],
+            "jira_state": "Closed",
+            "jira_resolution": "Won't Do",
+        }
+        workflow_reject = Workflow(
+            {
+                "name": "REJECTED",
+                "description": "random description",
+                "priority": 0,
+                "conditions": [],
+                "states": [state_reject],
+            }
+        )
+        workflow_framework.register_workflow(workflow)
+        workflow_framework.register_workflow(workflow_reject)
+
+        # 2) collect well formated CVEs
+        cc = CVEorgCollector()
+        cc.snippet_creation_enabled = True
+        cc.snippet_creation_start_date = None
+        cc.collect()
+
+        assert Snippet.objects.count() == 2
+        assert Flaw.objects.count() == 2
+
+        flaw1 = Flaw.objects.get(cve_id="CVE-2024-0181")
+        flaw1._create_or_update_task(jira_token)
+        snippet1 = Snippet.objects.get(external_id="CVE-2024-0181")
+        assert flaw1
+        assert snippet1
+        assert snippet1.flaw == flaw1
+
+        flaw2 = Flaw.objects.get(cve_id="CVE-2024-4923")
+        flaw2._create_or_update_task(jira_token)
+        snippet2 = Snippet.objects.get(external_id="CVE-2024-4923")
+        assert flaw2
+        assert snippet2
+        assert snippet2.flaw == flaw2
+
+        # 3) validate access control
+        assert flaw1.is_internal
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 404
+        response = auth_client().get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        flaw1_body = response.json()
+
+        assert flaw2.is_internal
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw2.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 404
+        response = auth_client().get(
+            f"{test_api_uri}/flaws/{flaw2.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        flaw2_body = response.json()
+
+        # 4) Test editing collected flaw
+        flaw_data = {
+            "title": "Spooky vulnerability",
+            "comment_zero": flaw1_body["comment_zero"],
+            "impact": "CRITICAL",
+            "components": ["curl"],
+            "source": "CVEORG",
+            "owner": "concosta@redhat.com",
+            "reported_dt": flaw1_body["reported_dt"],
+            "unembargo_dt": flaw1_body["unembargo_dt"],
+            "updated_dt": flaw1_body["updated_dt"],
+            "embargoed": False,
+        }
+        response = auth_client().put(
+            f"{test_api_uri}/flaws/{flaw1.uuid}",
+            flaw_data,
+            format="json",
+            HTTP_BUGZILLA_API_KEY=bugzilla_token,
+            HTTP_JIRA_API_KEY=jira_token,
+        )
+        assert response.status_code == 200
+
+        # 4.1) validate flaw still in NEW state
+        response = auth_client().get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        body = response.json()
+        assert body["classification"]["state"] == WorkflowModel.WorkflowState.NEW
+
+        # 4.2) validate access control
+        flaw1.refresh_from_db()
+        flaw1._create_or_update_task(jira_token)
+        assert flaw1.is_internal
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 404
+
+        # 5) Validate workflow
+        response = auth_client().post(
+            f"{test_api_uri}/flaws/{flaw1.uuid}/promote",
+            format="json",
+            HTTP_BUGZILLA_API_KEY=bugzilla_token,
+            HTTP_JIRA_API_KEY=jira_token,
+        )
+        assert response.status_code == 200
+        flaw1.refresh_from_db()
+        flaw1._create_or_update_task(jira_token)
+        assert flaw1.workflow_state == WorkflowModel.WorkflowState.TRIAGE
+        assert flaw1.is_internal
+
+        # 5.1) validate flaw is promoted
+        response = auth_client().get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        body = response.json()
+        assert body["classification"]["state"] == WorkflowModel.WorkflowState.TRIAGE
+
+        # 5.2) validate access control
+        flaw1.refresh_from_db()
+        flaw1._create_or_update_task(jira_token)
+        assert flaw1.is_internal
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 404
+
+        # 5.3) Validate promotion to secondary assessment
+        response = auth_client().post(
+            f"{test_api_uri}/flaws/{flaw1.uuid}/promote",
+            format="json",
+            HTTP_BUGZILLA_API_KEY=bugzilla_token,
+            HTTP_JIRA_API_KEY=jira_token,
+        )
+        assert response.status_code == 200
+        flaw1.refresh_from_db()
+        flaw1._create_or_update_task(jira_token)
+        assert flaw1.workflow_state == WorkflowModel.WorkflowState.SECONDARY_ASSESSMENT
+
+        # TODO remove this manual call after async task sync is calling it automatically
+        flaw1.adjust_acls(save=False)
+        flaw1.save(raise_validation_error=False)
+
+        assert not flaw1.is_internal
+        assert flaw1.is_public
+
+        # 5.4) validate flaw is promoted and publicly accessible
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        body = response.json()
+        assert (
+            body["classification"]["state"]
+            == WorkflowModel.WorkflowState.SECONDARY_ASSESSMENT
+        )
+
+        # 5.4) Validate promotion to done
+        response = auth_client().post(
+            f"{test_api_uri}/flaws/{flaw1.uuid}/promote",
+            format="json",
+            HTTP_BUGZILLA_API_KEY=bugzilla_token,
+            HTTP_JIRA_API_KEY=jira_token,
+        )
+        assert response.status_code == 200
+        flaw1.refresh_from_db()
+        flaw1._create_or_update_task(jira_token)
+        assert flaw1.workflow_state == WorkflowModel.WorkflowState.DONE
+        assert not flaw1.is_internal
+        assert flaw1.is_public
+
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw1.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        body = response.json()
+        assert body["classification"]["state"] == WorkflowModel.WorkflowState.DONE
+
+        # 6) Test editing rejected collected flaw
+        flaw_data = {
+            "title": "Not a vulnerability",
+            "comment_zero": flaw2_body["comment_zero"],
+            "impact": "LOW",
+            "components": ["curl"],
+            "source": "CVEORG",
+            "reported_dt": flaw2_body["reported_dt"],
+            "unembargo_dt": flaw2_body["unembargo_dt"],
+            "updated_dt": flaw2_body["updated_dt"],
+            "embargoed": False,
+        }
+        response = auth_client().put(
+            f"{test_api_uri}/flaws/{flaw2.uuid}",
+            flaw_data,
+            format="json",
+            HTTP_BUGZILLA_API_KEY=bugzilla_token,
+            HTTP_JIRA_API_KEY=jira_token,
+        )
+        assert response.status_code == 200
+        flaw2.refresh_from_db()
+        flaw2._create_or_update_task(jira_token)
+        assert flaw2.is_internal
+
+        # 6.1) Test rejecting collected flaw
+        response = auth_client().post(
+            f"{test_api_uri}/flaws/{flaw2.uuid}/reject",
+            data={"reason": "This was a spam."},
+            format="json",
+            HTTP_BUGZILLA_API_KEY=bugzilla_token,
+            HTTP_JIRA_API_KEY=jira_token,
+        )
+        body = response.json()
+        assert response.status_code == 200
+        assert body["classification"]["workflow"] == "REJECTED"
+        assert body["classification"]["state"] == WorkflowModel.WorkflowState.REJECTED
+        flaw2.refresh_from_db()
+        flaw2._create_or_update_task(jira_token)
+        assert flaw2.is_internal
+
+        # 6.2) Test access control
+        response = client.get(
+            f"{test_api_uri}/flaws/{flaw2.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 404
+        response = auth_client().get(
+            f"{test_api_uri}/flaws/{flaw2.uuid}?include_meta_attr=bz_id&include_history=true"
+        )
+        assert response.status_code == 200
+        body = response.json()
+        assert body["classification"]["state"] == WorkflowModel.WorkflowState.REJECTED
