8453bf997a4b981964812d5ad625daa01d7e1ecb
osoukup <osoukup@redhat.com>
2025-01-28 15:41:27 +0100

    extend CustomQuerySetUpdatedDt.update to allow not touching the updated_dt at all

--- osidb/query_sets.py
+++ osidb/query_sets.py
@@ -5,9 +5,13 @@ from django.utils import timezone
 class CustomQuerySetUpdatedDt(models.QuerySet):
     """Extend QuerySet to inject updated_dt on update"""
 
-    def update(self, **kwargs):
-        if getattr(self.model, "updated_dt", False):
-            if "updated_dt" not in kwargs:
-                kwargs["updated_dt"] = timezone.now().replace(microsecond=0)
+    def update(self, auto_timestamps=True, **kwargs):
+        """
+        if auto_timestamps is True and updated_dt is NOT present in kwargs updated_dt is set to now
+        """
+        if auto_timestamps:
+            if getattr(self.model, "updated_dt", False):
+                if "updated_dt" not in kwargs:
+                    kwargs["updated_dt"] = timezone.now().replace(microsecond=0)
 
         return super().update(**kwargs)
