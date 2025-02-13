17d078e5e38baea658cb1a3448765bd7d45b9232
Jitka Obselkova <jobselko@redhat.com>
2025-02-13 13:48:51 +0100

    Update docstring to cover CVSSv4

--- collectors/nvd/collectors.py
+++ collectors/nvd/collectors.py
@@ -68,7 +68,7 @@ class NVDQuerier:
         def get_cvss_metric(data: CVE, version: str) -> Union[dict, None]:
             """
             Return CVSS metric from `data` for the given `version`.
-            `version` can be of 3 values: cvssMetricV2, cvssMetricV30, cvssMetricV31.
+            `version` can be of 4 values: cvssMetricV2, cvssMetricV30, cvssMetricV31, cvssMetricV40.
             """
             # depending on the data, "metrics" and "version" might not be present
             if "metrics" in data and version in data.metrics:
