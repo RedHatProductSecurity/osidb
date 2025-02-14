22777fdee3dd5bda2a0f36ce5cc52ed66555c0e2
superbuggy <jpqreichard@gmail.com>
2025-02-14 13:00:42 -0500

    Revert "Map user name or email to pghistory context, replacing integer user id  (#900)"
    This reverts commit 6139e55bc34ea6a7fcd40edb7ab4cfe12e68549d, reversing
    changes made to 17e7583837951fda562a8a8d0021cdbc775679ec.

--- osidb/middleware.py
+++ osidb/middleware.py
@@ -3,12 +3,9 @@
     For a user to access rows in a table it must have the correct acl.
 
 """
-
 import logging
 
-import pghistory
 from django.conf import settings
-from django.core.handlers.wsgi import WSGIRequest
 
 from osidb.core import set_user_acls
 
@@ -35,44 +32,3 @@ class PgCommon:
             _logger.info(f"ACLs set from middleware for user {request.user.username}")
             set_user_acls(request.user.groups.all())
         return self.get_response(request)
-
-
-def get_userstr(user):
-    userstr = str(user)
-    if getattr(user, "is_authenticated", False):
-        if getattr(user, "email", False):
-            userstr = user.email
-        elif getattr(user, "username", False):
-            userstr = user.username
-    return userstr
-
-
-class WSGIRequestWithHook(WSGIRequest):
-    """
-    Updates pghistory context when the request.user attribute is updated in order
-    for context to be set before DB User-model lookup happens. User is added to the request
-    by Django in middleware, but added to the request object in the view layer by
-    django-rest-framework.
-    """
-
-    def __setattr__(self, attr, value):
-        _value = get_userstr(value) if attr == "user" else value
-        return super().__setattr__(attr, _value)
-
-
-class OsidbHistoryMiddleware:
-    """
-    Provides pghistory context with user email or name for use in FlawAudit model
-    """
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if not request or not hasattr(request, "user"):
-            return self.get_response(request)
-
-        with pghistory.context(user=get_userstr(request.user), path=request.path):
-            if isinstance(request, WSGIRequest):
-                request.__class__ = WSGIRequestWithHook
-            return self.get_response(request)
