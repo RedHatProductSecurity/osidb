5ed8966d1f53b27df0e09824d128350482feee96
Daniel Monzonis <dmonzoni@redhat.com>
2025-03-27 10:36:36 -0500

    Consider affect in AFFECTED/DELEGATED state as not resolved
    In OSIDB-4058 the affect resolution date was introduced, and only
    affects in the NEW:NOVALUE state were considered as not resolved.
    However, affects in AFFECTED:DELEGATED state should also be considered
    as not resolved and not have a `resolved_dt`.
    
    This commit marks those affects as not resolved as well, and a data
    migration removes the `resolved_dt` from the affects that already had
    one in these circumstances.
    
    Closes OSIDB-4137.

--- osidb/models/tests/test_affect.py
+++ osidb/models/tests/test_affect.py
@@ -1,9 +1,47 @@
+import pytest
+
 from osidb.models import Affect, Impact
 from osidb.tests.factories import AffectFactory, FlawFactory, PsModuleFactory
 
 
 class TestAffect:
-    def test_resolved_dt_auto_update(self):
+    @pytest.mark.parametrize(
+        "affectedness,resolution,is_resolved",
+        [
+            (Affect.AffectAffectedness.NEW, Affect.AffectResolution.NOVALUE, False),
+            (
+                Affect.AffectAffectedness.AFFECTED,
+                Affect.AffectResolution.DELEGATED,
+                False,
+            ),
+            (Affect.AffectAffectedness.NEW, Affect.AffectResolution.WONTFIX, True),
+            (Affect.AffectAffectedness.AFFECTED, Affect.AffectResolution.WONTFIX, True),
+        ],
+    )
+    @pytest.mark.parametrize(
+        # Second set of values to use for when updating the affectedness/resolution,
+        # so that the possible combinations are tested
+        "affectedness_2,resolution_2,is_resolved_2",
+        [
+            (Affect.AffectAffectedness.NEW, Affect.AffectResolution.NOVALUE, False),
+            (
+                Affect.AffectAffectedness.AFFECTED,
+                Affect.AffectResolution.DELEGATED,
+                False,
+            ),
+            (Affect.AffectAffectedness.NEW, Affect.AffectResolution.WONTFIX, True),
+            (Affect.AffectAffectedness.AFFECTED, Affect.AffectResolution.WONTFIX, True),
+        ],
+    )
+    def test_resolved_dt_auto_update(
+        self,
+        affectedness,
+        resolution,
+        is_resolved,
+        affectedness_2,
+        resolution_2,
+        is_resolved_2,
+    ):
         """
         test resolve_dt is null when an affect is unresolved and is
         automatically updated when first entered in a resolved state
@@ -13,74 +51,32 @@ class TestAffect:
         # Check factory behavior
         affect = AffectFactory(
             flaw=flaw,
-            affectedness=Affect.AffectAffectedness.NEW,
-            resolution=Affect.AffectResolution.NOVALUE,
+            affectedness=affectedness,
+            resolution=resolution,
         )
-        assert not affect.is_resolved
-        assert not affect.resolved_dt
-
-        affect = AffectFactory(
-            flaw=flaw,
-            affectedness=Affect.AffectAffectedness.NEW,
-            resolution=Affect.AffectResolution.WONTFIX,
-        )
-        assert affect.is_resolved
-        assert affect.resolved_dt
+        assert affect.is_resolved == is_resolved
+        assert affect.resolved_dt or not is_resolved
 
         ps_module = PsModuleFactory(name="test-module")
 
         # Check creation behavior
-        affect_unresolved = Affect(
+        affect = Affect(
             impact=Impact.MODERATE,
             flaw=flaw,
-            affectedness=Affect.AffectAffectedness.NEW,
-            resolution=Affect.AffectResolution.NOVALUE,
+            affectedness=affectedness,
+            resolution=resolution,
             ps_component="component-10",
             ps_module=ps_module.name,
             acl_read=flaw.acl_read,
             acl_write=flaw.acl_write,
         )
-        affect_unresolved.save()
+        affect.save()
+        assert affect.is_resolved == is_resolved
+        assert affect.resolved_dt or not is_resolved
 
-        assert not affect_unresolved.is_resolved
-        assert not affect_unresolved.resolved_dt
-
-        affect_resolved = Affect(
-            impact=Impact.MODERATE,
-            flaw=flaw,
-            affectedness=Affect.AffectAffectedness.AFFECTED,
-            resolution=Affect.AffectResolution.DELEGATED,
-            ps_component="component-20",
-            ps_module=ps_module.name,
-            acl_read=flaw.acl_read,
-            acl_write=flaw.acl_write,
-        )
-        affect_resolved.save()
-
-        assert affect_resolved.is_resolved
-        assert affect_resolved.resolved_dt
-
-        # Check update behavior from unresolved to resolved
-        affect_unresolved.affectedness = Affect.AffectAffectedness.AFFECTED
-        affect_unresolved.resolution = Affect.AffectResolution.DELEGATED
-        affect_unresolved.save()
-
-        assert affect_unresolved.is_resolved
-        assert affect_unresolved.resolved_dt
-
-        # Check update behavior from resolved to resolved
-        current_resolved_dt = affect_resolved.resolved_dt
-        affect_resolved.affectedness = Affect.AffectAffectedness.AFFECTED
-        affect_resolved.resolution = Affect.AffectResolution.WONTFIX
-        affect_resolved.save()
-
-        assert affect_resolved.is_resolved
-        assert affect_resolved.resolved_dt == current_resolved_dt
-
-        # Check update behavior from resolved to unresolved
-        affect_resolved.affectedness = Affect.AffectAffectedness.NEW
-        affect_resolved.resolution = Affect.AffectResolution.NOVALUE
-        affect_resolved.save()
-
-        assert not affect_resolved.is_resolved
-        assert not affect_resolved.resolved_dt
+        # Check update behavior
+        affect.affectedness = affectedness_2
+        affect.resolution = resolution_2
+        affect.save()
+        assert affect.is_resolved == is_resolved_2
+        assert affect.resolved_dt or not is_resolved_2
