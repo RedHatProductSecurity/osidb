8de5a290a53803549d5055947e516321e357415c
Jon Moroney <darakian@redhat.com>
2025-07-24 14:59:38 -0700

    Remove cassette usage. Tests still pass

--- collectors/cveorg/tests/test_collectors.py
+++ collectors/cveorg/tests/test_collectors.py
@@ -78,10 +78,6 @@ class TestCVEorgCollector:
         assert not Flaw.objects.filter(cve_id="CVE-2024-1087")  # rejected flaw
 
 
-    @pytest.mark.vcr
-    @pytest.mark.default_cassette(
-        "TestCVEorgCollector.test_collect_cveorg_records.yaml"
-    )
     def test_ignored_cve_with_blocked_cna_assignerid(self, mock_keywords, mock_repo):
         """
         Test that snippets and flaws are not created when the assigning cna is blocked by ps_constants
