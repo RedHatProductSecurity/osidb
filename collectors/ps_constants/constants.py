1a140a0758c23308a287271bcffaac5c0f52452b
Conrado Costa <concosta@redhat.com>
2025-10-01 11:35:34 -0400

    squash

--- collectors/ps_constants/constants.py
+++ collectors/ps_constants/constants.py
@@ -5,4 +5,4 @@ product definitions collector constants
 from osidb.helpers import get_env
 
 PS_CONSTANTS_REPO_URL = get_env("PS_CONSTANTS_URL")
-PS_CONSTANTS_REPO_BRANCH = "master"
+PS_CONSTANTS_REPO_BRANCH = "sla-policies"
