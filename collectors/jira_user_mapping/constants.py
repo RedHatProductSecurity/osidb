6398d8eaa2e7334da5e0242a4e799a459cfeed27
Jakub Frejlach <jfrejlac@redhat.com>
2026-02-26 18:28:57 +0100

    Implement collector for Jira user mapping

---
+++ collectors/jira_user_mapping/constants.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class JiraUserMappingCollectorSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="JIRA_USER_MAPPING_COLLECTOR_")
+
+    enabled: bool = False
+    url: str = ""
+    crontab_hour: int = 3
+
+
+jira_user_mapping_collector_settings = JiraUserMappingCollectorSettings()
