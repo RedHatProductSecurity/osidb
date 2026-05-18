8a5dc740babf2a98f61163720a827f5d17c83270
osoukup <osoukup@redhat.com>
2026-05-18 10:08:56 +0200

    deprecate no more used workflow functionality

--- apps/workflows/exceptions.py
+++ apps/workflows/exceptions.py
@@ -11,24 +11,8 @@ class APIError(WorkflowsException):
     """exception class for API errors"""
 
 
-class LastStateException(WorkflowsException):
-    """exception for trying to promote further when in the last possible state"""
-
-
-class InitialStateException(WorkflowsException):
-    """exception for trying to revert further when in the first possible state"""
-
-
-class MissingRequirementsException(WorkflowsException):
-    """exception for trying to change state without requirements"""
-
-
 class MissingStateException(WorkflowsException):
     """exception for handling a non-registered state"""
-
-
-class MissingWorkflowException(WorkflowsException):
-    """exception for handling a non-registered workflow"""
 
 
 class WorkflowDefinitionError(WorkflowsException):
