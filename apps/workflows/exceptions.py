74b4b709ca0914424afe3ad3ff0c1b186c400180
osoukup <osoukup@redhat.com>
2026-05-25 14:00:02 +0200

    simplify jira_status in alignment to the automatic classification

--- apps/workflows/exceptions.py
+++ apps/workflows/exceptions.py
@@ -11,9 +11,5 @@ class APIError(WorkflowsException):
     """exception class for API errors"""
 
 
-class MissingStateException(WorkflowsException):
-    """exception for handling a non-registered state"""
-
-
 class WorkflowDefinitionError(WorkflowsException):
     """exception class for workflow definitions errors"""
