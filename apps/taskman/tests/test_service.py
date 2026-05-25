0e5fd0d28d6d90a34d20d212f771b3e76b23d16a
osoukup <osoukup@redhat.com>
2026-05-25 17:20:29 +0200

    align the DEFAULT workflow with the workflow framework changes

--- apps/taskman/tests/test_service.py
+++ apps/taskman/tests/test_service.py
@@ -48,10 +48,10 @@ class TestTaskmanService(object):
         assert response2 is None
 
         assert flaw.workflow_state in (
+            WorkflowModel.WorkflowState.PRE_SECONDARY_ASSESSMENT,
             WorkflowModel.WorkflowState.SECONDARY_ASSESSMENT,
             WorkflowModel.WorkflowState.DONE,
         )
-        flaw.workflow_state = WorkflowModel.WorkflowState.PRE_SECONDARY_ASSESSMENT
         flaw.save(raise_validation_error=False)
         response3 = taskman.create_or_update_task(flaw=flaw)
         assert response3 is None
