da6b895f1ea41d310d51f58cacbff157feb6439d
osoukup <osoukup@redhat.com>
2026-06-17 15:38:24 +0200

    allow conditions to be used as workflow conditions

--- apps/workflows/models.py
+++ apps/workflows/models.py
@@ -96,7 +96,8 @@ class State:
             for requirement_desc in state_desc["requirements"]
         ]
 
-    def parse_requirement(self, requirement_desc):
+    @staticmethod
+    def parse_requirement(requirement_desc):
         """
         Parse a requirement, which can be a regular check or a logical condition involving
         several checks.
@@ -122,12 +123,20 @@ class Workflow:
     provides classification of the instance in the proper state
     """
 
+    @staticmethod
+    def parse_check(check_desc):
+        if isinstance(check_desc, str):
+            return Check(check_desc)
+        if isinstance(check_desc, dict):
+            return Condition(**check_desc)
+        raise ValueError(f"Invalid check format in workflow: {check_desc}")
+
     def __init__(self, workflow_desc):
         self.name = workflow_desc["name"]
         self.description = workflow_desc["description"]
         self.priority = int(workflow_desc["priority"])
         self.conditions = [
-            Check(requirement_desc) for requirement_desc in workflow_desc["conditions"]
+            self.parse_check(check_desc) for check_desc in workflow_desc["conditions"]
         ]
         self.states = [State(state_desc) for state_desc in workflow_desc["states"]]
 
