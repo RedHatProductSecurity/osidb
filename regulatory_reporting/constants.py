dd58571ca6b27d86cef3b6d058b4d48080d856a1
Rodrigo Alejandro Duran <roduran@redhat.com>
2026-07-03 16:38:02 +0200

    OSIDB-5073: Add srp enpoints endpoints

---
+++ regulatory_reporting/constants.py
@@ -0,0 +1,6 @@
+"""Shared constants for regulatory reporting."""
+
+# UUID path segment for DRF router nested kwargs and lookup_value_regex.
+UUID_PATH_REGEX = (
+    r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+)
